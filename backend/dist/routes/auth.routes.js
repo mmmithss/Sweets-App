185cc73d3cbd27a354ce53d9b4026b4ab0312629
mmithss <nrmenonm@gmail.com>
2025-09-20 13:42:25 +0530

    minor updates

--- backend/dist/routes/auth.routes.js
+++ backend/dist/routes/auth.routes.js
@@ -1,11 +1,10 @@
 import Router from "express";
 import { getAllAccounts, login, logout, signup } from "../controllers/auth.controller.js";
-import { get } from "http";
 const authRoutes = Router();
 //todo priority
 authRoutes.post("/register", signup);
 authRoutes.post("/login", login);
-authRoutes.get("me");
+authRoutes.get("/me");
 //TODO : Later
 authRoutes.post("/logout", logout);
 authRoutes.get("allAccounts", getAllAccounts);
